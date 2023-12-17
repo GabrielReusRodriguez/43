1bb5bd2f861389f662c7dc3db56649a656e1c72b
Gabriel Reus Rodriguez <gabriel.reusrodriguez@gmail.com>
2023-12-17 23:13:38 +0100

    Añadido scripts de instalacion de mini moulinette y paco

---
+++ scripts/francinette_install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+bash -c "$(curl -fsSL https://raw.github.com/xicodomingues/francinette/master/bin/install.sh)"
+
---
+++ scripts/minimoulinette_install.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env  bash
+
+echo "alias mini='~/mini-moulinette/mini-moul.sh'" >> ~/.bashrc && source ~/.bashrc
